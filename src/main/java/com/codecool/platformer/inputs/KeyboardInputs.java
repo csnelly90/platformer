32c449322b10d569c1d855e15900432d449a9549
csnelly90 <csnelly90@gmail.com>
2023-01-25 18:53:40 +0100

    add inputs package and KeyboardInputs class

---
+++ src/main/java/com/codecool/platformer/inputs/KeyboardInputs.java
@@ -0,0 +1,22 @@
+package com.codecool.platformer.inputs;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyboardInputs implements KeyListener {
+
+    @Override
+    public void keyTyped(KeyEvent keyEvent) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent keyEvent) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent keyEvent) {
+
+    }
+}
