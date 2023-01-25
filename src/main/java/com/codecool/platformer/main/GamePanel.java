f1d607aba0bfaa6ca4cebf2c80bf027d0a8ab982
csnelly90 <csnelly90@gmail.com>
2023-01-25 19:14:10 +0100

    add mouse event listeners to gamePanel

--- src/main/java/com/codecool/platformer/main/GamePanel.java
+++ src/main/java/com/codecool/platformer/main/GamePanel.java
@@ -1,14 +1,20 @@
 package com.codecool.platformer.main;
 
 import com.codecool.platformer.inputs.KeyboardInputs;
+import com.codecool.platformer.inputs.MouseInputs;
 
 import javax.swing.*;
 import java.awt.*;
 
 public class GamePanel extends JPanel {
 
+    private MouseInputs mouseInputs;
+
     public GamePanel() {
+        this.mouseInputs = new MouseInputs();
         addKeyListener(new KeyboardInputs());
+        addMouseListener(mouseInputs);
+        addMouseMotionListener(mouseInputs);
     }
 
     public void paintComponent(Graphics g) {
