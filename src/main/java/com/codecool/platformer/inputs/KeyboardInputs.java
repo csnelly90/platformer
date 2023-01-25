818d99b6941da5bfa49ea84a03fe1b270863f287
csnelly90 <csnelly90@gmail.com>
2023-01-25 19:35:16 +0100

    add gamePanel to KeyboardInputs and implement WASD movement

--- src/main/java/com/codecool/platformer/inputs/KeyboardInputs.java
+++ src/main/java/com/codecool/platformer/inputs/KeyboardInputs.java
@@ -1,9 +1,17 @@
 package com.codecool.platformer.inputs;
 
+import com.codecool.platformer.main.GamePanel;
+
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class KeyboardInputs implements KeyListener {
+
+    private GamePanel gamePanel;
+
+    public KeyboardInputs(GamePanel gamePanel) {
+        this.gamePanel = gamePanel;
+    }
 
     @Override
     public void keyTyped(KeyEvent e) {
@@ -12,18 +20,18 @@ public class KeyboardInputs implements KeyListener {
 
     @Override
     public void keyPressed(KeyEvent e) {
-        switch(e.getKeyCode()) {
+        switch (e.getKeyCode()) {
             case KeyEvent.VK_W:
-                System.out.println("W pressed");
+                gamePanel.changeYDelta(-5);
                 break;
             case KeyEvent.VK_A:
-                System.out.println("A pressed");
+                gamePanel.changeXDelta(-5);
                 break;
             case KeyEvent.VK_S:
-                System.out.println("S pressed");
+                gamePanel.changeYDelta(5);
                 break;
             case KeyEvent.VK_D:
-                System.out.println("D pressed");
+                gamePanel.changeXDelta(5);
                 break;
         }
     }
--- src/main/java/com/codecool/platformer/main/GamePanel.java
+++ src/main/java/com/codecool/platformer/main/GamePanel.java
@@ -9,15 +9,27 @@ import java.awt.*;
 public class GamePanel extends JPanel {
 
     private MouseInputs mouseInputs;
+    private int xDelta = 0, yDelta = 0;
 
     public GamePanel() {
         this.mouseInputs = new MouseInputs();
-        addKeyListener(new KeyboardInputs());
+        addKeyListener(new KeyboardInputs(this));
         addMouseListener(mouseInputs);
         addMouseMotionListener(mouseInputs);
     }
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
+        g.fillRect(100 + xDelta, 100 + yDelta, 20, 20);
+    }
+
+    public void changeXDelta(int value) {
+        xDelta += value;
+        repaint();
+    }
+
+    public void changeYDelta(int value) {
+        yDelta += value;
+        repaint();
     }
 }
