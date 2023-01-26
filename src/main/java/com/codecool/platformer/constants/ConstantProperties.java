7fea61526845ca5696200614eab752dd66b04731
csnelly90 <csnelly90@gmail.com>
2023-01-26 01:36:34 +0100

    add ConstantProperties class

---
+++ src/main/java/com/codecool/platformer/constants/ConstantProperties.java
@@ -0,0 +1,20 @@
+package com.codecool.platformer.constants;
+
+public final class ConstantProperties {
+
+    public static final int WINDOW_WIDTH = 1280;
+    public static final int WINDOW_HEIGHT = 800;
+
+    private ConstantProperties() {}
+
+    public enum SpriteSize {
+        PLAYER(64, 40);
+        public final int WIDTH;
+        public final int HEIGHT;
+
+        SpriteSize(int width, int height) {
+            this.WIDTH = width;
+            this.HEIGHT = height;
+        }
+    }
+}
