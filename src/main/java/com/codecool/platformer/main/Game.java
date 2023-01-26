3c34dc30e2724a96397c7b321b1f93decf7319ec
csnelly90 <csnelly90@gmail.com>
2023-01-26 16:12:41 +0100

    enhance game loop by separating the calculations for game logic updates and graphics rendering

--- src/main/java/com/codecool/platformer/main/Game.java
+++ src/main/java/com/codecool/platformer/main/Game.java
@@ -1,13 +1,15 @@
 package com.codecool.platformer.main;
 
+import com.codecool.platformer.constants.GameProperties;
+
 public class Game implements Runnable {
 
     private GameWindow gameWindow;
     private GamePanel gamePanel;
     private Thread gameThread;
-    private final int FPS_SET = 120;
     private final int ONE_SEC_IN_MILLISECONDS = 1000;
     private final double ONE_SEC_IN_NANOSECONDS = 1000000000.0;
+
 
     public Game() {
         this.gamePanel = new GamePanel();
@@ -21,27 +23,49 @@ public class Game implements Runnable {
         gameThread.start();
     }
 
+    private void update() {
+        gamePanel.updateGame();
+    }
+
     @Override
     public void run() {
-        double timePerFrame = ONE_SEC_IN_NANOSECONDS / FPS_SET;
-        long lastFrame = System.nanoTime();
-        long now = System.nanoTime();
+        double timePerFrame = ONE_SEC_IN_NANOSECONDS / GameProperties.FPS_PER_SEC;
+        double timePerUpdate = ONE_SEC_IN_NANOSECONDS / GameProperties.UPS_PER_SEC;
+        long previousTime = System.nanoTime();
         int frames = 0;
+        int updates = 0;
         long lastCheck = System.currentTimeMillis();
 
-        while (true) {
-            now = System.nanoTime();
+        // delta times for updates and frames
+        double deltaUpdate = 0;
+        double deltaFrames = 0;
 
-            if (now - lastFrame >= timePerFrame) {
+        while (true) {
+            long currentTime = System.nanoTime();
+
+            // make use of the time we lose when calculating FPS
+            deltaUpdate += (currentTime - previousTime) / timePerUpdate;
+            deltaFrames += (currentTime - previousTime) / timePerFrame;
+            previousTime = currentTime;
+
+            // next update comes faster if we have more than 1 sec of lost time in total
+            if (deltaUpdate >= 1) {
+                update();
+                updates++;
+                deltaUpdate--;
+            }
+
+            if (deltaFrames >= 1) {
                 gamePanel.repaint();
-                lastFrame = now;
                 frames++;
+                deltaFrames--;
             }
 
             if (System.currentTimeMillis() - lastCheck >= ONE_SEC_IN_MILLISECONDS) {
                 lastCheck = System.currentTimeMillis();
-                System.out.println("FPS: " + frames);
+                System.out.println("FPS: " + frames + " | UPS: " + updates);
                 frames = 0;
+                updates = 0;
             }
         }
     }
