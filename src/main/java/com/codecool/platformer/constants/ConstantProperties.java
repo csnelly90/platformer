75a04556a2f9f0662d21b91dc0d55cd1150df81d
csnelly90 <csnelly90@gmail.com>
2023-01-26 01:56:33 +0100

    add FPS and animation per sec to ConstantProperties

--- src/main/java/com/codecool/platformer/constants/ConstantProperties.java
+++ src/main/java/com/codecool/platformer/constants/ConstantProperties.java
@@ -4,6 +4,8 @@ public final class ConstantProperties {
 
     public static final int WINDOW_WIDTH = 1280;
     public static final int WINDOW_HEIGHT = 800;
+    public static final int ANIMATION_PER_SEC = 8;
+    public static final int FPS_PER_SEC = 120;
 
     private ConstantProperties() {}
 
