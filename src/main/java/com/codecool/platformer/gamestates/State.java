3c756ba78e1a2af75031969ab6634320a902c51f
csnelly90 <csnelly90@gmail.com>
2023-02-08 20:03:03 +0100

    add helper method in State class to check if cursor is inside button

--- src/main/java/com/codecool/platformer/gamestates/State.java
+++ src/main/java/com/codecool/platformer/gamestates/State.java
@@ -1,12 +1,19 @@
 package com.codecool.platformer.gamestates;
 
 import com.codecool.platformer.main.Game;
+import com.codecool.platformer.ui.MenuButton;
+
+import java.awt.event.MouseEvent;
 
 public class State {
     protected Game game;
 
     public State(Game game) {
         this.game = game;
+    }
+
+    public boolean isInsideButton(MouseEvent e, MenuButton mb) {
+        return mb.getHitbox().contains(e.getX(), e.getY());
     }
 
     public Game getGame() {
