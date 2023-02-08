433a6598b0895fc74338012e6629d1f566990077
csnelly90 <csnelly90@gmail.com>
2023-02-08 20:00:05 +0100

    change game scale to bigger value

--- src/main/java/com/codecool/platformer/constants/GameProperties.java
+++ src/main/java/com/codecool/platformer/constants/GameProperties.java
@@ -6,7 +6,7 @@ public final class GameProperties {
     public static final int FPS_PER_SEC = 120;
     public static final int UPS_PER_SEC = 200;
     public static final int DEFAULT_TILE_SIZE = 32;
-    public static final float SCALE = 1.5f;
+    public static final float SCALE = 2.5f;
     public static final int TILES_HORIZONTAL = 26;
     public static final int TILES_VERTICAL = 14;
     public static final int TILE_SIZE = (int) (DEFAULT_TILE_SIZE * SCALE);
