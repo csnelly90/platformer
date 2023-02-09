1119eed27b15d2a5c501a0e8b84a4f4ae1643c20
csnelly90 <csnelly90@gmail.com>
2023-02-09 13:17:15 +0100

    add PauseButton parent class

---
+++ src/main/java/com/codecool/platformer/ui/PauseButton.java
@@ -0,0 +1,63 @@
+package com.codecool.platformer.ui;
+
+import java.awt.*;
+
+import static com.codecool.platformer.constants.GameProperties.UI.Buttons.MB_HEIGHT;
+import static com.codecool.platformer.constants.GameProperties.UI.Buttons.MB_WIDTH;
+
+public class PauseButton {
+    protected int x, y, width, height;
+    protected Rectangle hitbox;
+
+    public PauseButton(int x, int y, int width, int height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        initHitbox();
+    }
+
+    private void initHitbox() {
+        hitbox = new Rectangle(x, y, width, height);
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public Rectangle getHitbox() {
+        return hitbox;
+    }
+
+    public void setHitbox(Rectangle hitbox) {
+        this.hitbox = hitbox;
+    }
+}
