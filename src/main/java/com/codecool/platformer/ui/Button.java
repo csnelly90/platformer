9e7cb7fef787603b696bc08194f8b0e3ad88cb13
csnelly90 <csnelly90@gmail.com>
2023-02-09 15:01:25 +0100

    add Button interface

---
+++ src/main/java/com/codecool/platformer/ui/Button.java
@@ -0,0 +1,10 @@
+package com.codecool.platformer.ui;
+
+import java.awt.*;
+
+public interface Button {
+    public void resetMouseState();
+    public void draw(Graphics g);
+    public void update();
+
+}
