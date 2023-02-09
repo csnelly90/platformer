996eced2a7bb2e7c76facfa72461f151f82a49b0
csnelly90 <csnelly90@gmail.com>
2023-02-09 14:29:14 +0100

    implement loadSoundButtons method in PauseOverlay

--- src/main/java/com/codecool/platformer/ui/PauseOverlay.java
+++ src/main/java/com/codecool/platformer/ui/PauseOverlay.java
@@ -7,12 +7,25 @@ import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
 
+import static com.codecool.platformer.constants.GameProperties.UI.PauseButtons.*;
+
 public class PauseOverlay {
     private BufferedImage background;
     private int menuX, menuY, menuWidth, menuHeight;
+    private SoundButton musicButton, sfxButton;
 
     public PauseOverlay() {
         loadBackground();
+        loadSoundButtons();
+    }
+
+    private void loadSoundButtons() {
+        int soundX = (int) (450 * GameProperties.SCALE);
+        int musicY = (int) (145 * GameProperties.SCALE);
+        int sfxY = (int) (190 * GameProperties.SCALE);
+
+        musicButton = new SoundButton(soundX, musicY, SOUND_BUTTON_SIZE, SOUND_BUTTON_SIZE);
+        sfxButton = new SoundButton(soundX, sfxY, SOUND_BUTTON_SIZE, SOUND_BUTTON_SIZE);
     }
 
     private void loadBackground() {
@@ -28,7 +41,12 @@ public class PauseOverlay {
     }
 
     public void draw(Graphics g) {
+        // background
         g.drawImage(background, menuX, menuY, menuWidth, menuHeight, null);
+
+        // sound buttons
+        musicButton.draw(g);
+        sfxButton.draw(g);
     }
 
     public void mouseDragged(MouseEvent e) {
