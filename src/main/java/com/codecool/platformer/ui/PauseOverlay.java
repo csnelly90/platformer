a62b7088077ca22564f84720537417ea1f3cb953
csnelly90 <csnelly90@gmail.com>
2023-02-09 11:05:52 +0100

    add PauseOverlay class to UI package

---
+++ src/main/java/com/codecool/platformer/ui/PauseOverlay.java
@@ -0,0 +1,17 @@
+package com.codecool.platformer.ui;
+
+import java.awt.*;
+
+public class PauseOverlay {
+
+    public PauseOverlay() {
+    }
+
+    public void update() {
+
+    }
+
+    public void draw(Graphics g) {
+
+    }
+}
