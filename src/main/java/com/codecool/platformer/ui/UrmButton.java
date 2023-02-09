7e45ed43cc4f21c6e63b456ff9ad6c60d0fbeaef
csnelly90 <csnelly90@gmail.com>
2023-02-09 15:28:00 +0100

    implement loadImages method in UrmButton

--- src/main/java/com/codecool/platformer/ui/UrmButton.java
+++ src/main/java/com/codecool/platformer/ui/UrmButton.java
@@ -1,11 +1,36 @@
 package com.codecool.platformer.ui;
 
+import com.codecool.platformer.utils.LoadSave;
+
 import java.awt.*;
+import java.awt.image.BufferedImage;
+
+import static com.codecool.platformer.constants.GameProperties.UI.PauseButtons.*;
 
 public class UrmButton extends PauseButton implements Button {
+    private BufferedImage[] images;
+    private boolean mouseOver, mousePressed;
+    private boolean muted;
+    private int rowIndex;
 
-    public UrmButton(int x, int y, int width, int height) {
+    public UrmButton(int x, int y, int width, int height, int rowIndex) {
         super(x, y, width, height);
+        this.rowIndex = rowIndex;
+        loadImages();
+    }
+
+    private void loadImages() {
+        images = new BufferedImage[3];
+        BufferedImage temp = LoadSave.getSpriteAtlas(LoadSave.URM_BUTTONS);
+
+        for (int i = 0; i < images.length; i++) {
+            images[i] = temp.getSubimage(
+                    i * URM_BUTTON_DEFAULT_SIZE,
+                    rowIndex * URM_BUTTON_DEFAULT_SIZE,
+                    URM_BUTTON_DEFAULT_SIZE,
+                    URM_BUTTON_DEFAULT_SIZE
+            );
+        }
     }
 
     @Override
