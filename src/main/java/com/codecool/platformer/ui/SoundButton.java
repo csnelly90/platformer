fd6c1ffb40ddd24c0d5e906374e3b3de966850fb
csnelly90 <csnelly90@gmail.com>
2023-02-09 14:28:28 +0100

    implement draw and update methods in SoundButton, add related fields with getters and setters

--- src/main/java/com/codecool/platformer/ui/SoundButton.java
+++ src/main/java/com/codecool/platformer/ui/SoundButton.java
@@ -2,12 +2,16 @@ package com.codecool.platformer.ui;
 
 import com.codecool.platformer.utils.LoadSave;
 
+import java.awt.*;
 import java.awt.image.BufferedImage;
 
 import static com.codecool.platformer.constants.GameProperties.UI.PauseButtons.*;
 
 public class SoundButton extends PauseButton {
     private BufferedImage[][] soundImages;
+    private boolean mouseOver, mousePressed;
+    private boolean muted;
+    private int rowIndex, colIndex;
 
     public SoundButton(int x, int y, int width, int height) {
         super(x, y, width, height);
@@ -28,5 +32,60 @@ public class SoundButton extends PauseButton {
                 );
             }
         }
+    }
+
+    public void update() {
+        // gets the first or second row of button sprites in the image
+        if (muted) {
+            rowIndex = 1;
+        } else {
+            rowIndex = 0;
+        }
+
+        // gets the proper button image from row based on mouse event
+        colIndex = 0;
+
+        if (mouseOver) {
+            colIndex = 1;
+        }
+
+        if (mousePressed) {
+            colIndex = 2;
+        }
+    }
+
+    public void draw(Graphics g) {
+        g.drawImage(
+                soundImages[rowIndex][colIndex],
+                x,
+                y,
+                width,
+                height,
+                null
+        );
+    }
+
+    public boolean isMouseOver() {
+        return mouseOver;
+    }
+
+    public void setMouseOver(boolean mouseOver) {
+        this.mouseOver = mouseOver;
+    }
+
+    public boolean isMousePressed() {
+        return mousePressed;
+    }
+
+    public void setMousePressed(boolean mousePressed) {
+        this.mousePressed = mousePressed;
+    }
+
+    public boolean isMuted() {
+        return muted;
+    }
+
+    public void setMuted(boolean muted) {
+        this.muted = muted;
     }
 }
