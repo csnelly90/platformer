9b1a854124a45286df00669b63fdb80a50c03a9c
csnelly90 <csnelly90@gmail.com>
2023-02-09 13:05:45 +0100

    implement loading of pause menu background

--- src/main/java/com/codecool/platformer/ui/PauseOverlay.java
+++ src/main/java/com/codecool/platformer/ui/PauseOverlay.java
@@ -1,11 +1,26 @@
 package com.codecool.platformer.ui;
 
+import com.codecool.platformer.constants.GameProperties;
+import com.codecool.platformer.utils.LoadSave;
+
 import java.awt.*;
 import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
 
 public class PauseOverlay {
+    private BufferedImage background;
+    private int menuX, menuY, menuWidth, menuHeight;
 
     public PauseOverlay() {
+        loadBackground();
+    }
+
+    private void loadBackground() {
+        background = LoadSave.getSpriteAtlas(LoadSave.PAUSE_BACKGROUND);
+        menuWidth = (int) (background.getWidth() * GameProperties.SCALE);
+        menuHeight = (int) (background.getHeight() * GameProperties.SCALE);
+        menuX = GameProperties.WINDOW_WIDTH / 2 - menuWidth / 2;
+        menuY = GameProperties.WINDOW_HEIGHT / 2 - menuHeight / 2;
     }
 
     public void update() {
@@ -13,7 +28,7 @@ public class PauseOverlay {
     }
 
     public void draw(Graphics g) {
-
+        g.drawImage(background, menuX, menuY, menuWidth, menuHeight, null);
     }
 
     public void mouseDragged(MouseEvent e) {
