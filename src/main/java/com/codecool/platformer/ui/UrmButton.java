9188ce60b7bd5dd96c61f8247cb92f7927ef97dd
csnelly90 <csnelly90@gmail.com>
2023-02-09 15:06:54 +0100

    add UrmButton class

---
+++ src/main/java/com/codecool/platformer/ui/UrmButton.java
@@ -0,0 +1,25 @@
+package com.codecool.platformer.ui;
+
+import java.awt.*;
+
+public class UrmButton extends PauseButton implements Button {
+
+    public UrmButton(int x, int y, int width, int height) {
+        super(x, y, width, height);
+    }
+
+    @Override
+    public void resetMouseState() {
+
+    }
+
+    @Override
+    public void draw(Graphics g) {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+}
