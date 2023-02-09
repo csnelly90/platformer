4dc319065b9dbca0125252ab600f1ed6e37aebdb
csnelly90 <csnelly90@gmail.com>
2023-02-09 11:05:32 +0100

    add boolean for paused gameplay in Playing class

--- src/main/java/com/codecool/platformer/gamestates/Playing.java
+++ src/main/java/com/codecool/platformer/gamestates/Playing.java
@@ -14,6 +14,7 @@ import java.awt.event.MouseEvent;
 public class Playing extends State implements StateMethods {
     private Player player;
     private LevelManager levelManager;
+    private boolean paused;
 
     public Playing(Game game) {
         super(game);
