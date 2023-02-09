657302880bb8a4be70bc687d7740eb92db291812
csnelly90 <csnelly90@gmail.com>
2023-02-09 15:06:42 +0100

    add urm buttons image path to LoadSave

--- src/main/java/com/codecool/platformer/utils/LoadSave.java
+++ src/main/java/com/codecool/platformer/utils/LoadSave.java
@@ -17,6 +17,7 @@ public class LoadSave {
     public static final String MENU_BACKGROUND = "/gui/menu_background.png";
     public static final String PAUSE_BACKGROUND = "/gui/pause_menu.png";
     public static final String SOUND_BUTTONS = "/gui/sound_buttons.png";
+    public static final String URM_BUTTONS = "/gui/urm_buttons.png";
 
     public static BufferedImage getSpriteAtlas(String fileName) {
         BufferedImage img = null;
