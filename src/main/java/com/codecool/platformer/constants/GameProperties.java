6eb6b459e45f5375fd5b73d75918696021d4e289
csnelly90 <csnelly90@gmail.com>
2023-02-12 18:49:56 +0100

    add size constant values for volume buttons in GameProperties

--- src/main/java/com/codecool/platformer/constants/GameProperties.java
+++ src/main/java/com/codecool/platformer/constants/GameProperties.java
@@ -29,7 +29,12 @@ public final class GameProperties {
             public static final int SOUND_BUTTON_SIZE = (int) (SOUND_BUTTON_DEFAULT_SIZE * SCALE);
             public static final int URM_BUTTON_DEFAULT_SIZE = 56;
             public static final int URM_BUTTON_SIZE = (int) (URM_BUTTON_DEFAULT_SIZE * SCALE);
-
+            public static final int VOL_BUTTON_DEFAULT_WIDTH = 28;
+            public static final int VOL_BUTTON_DEFAULT_HEIGHT = 45;
+            public static final int VOL_SLIDER_DEFAULT_WIDTH = 215;
+            public static final int VOL_BUTTON_WIDTH = (int) (VOL_BUTTON_DEFAULT_WIDTH * SCALE);
+            public static final int VOL_BUTTON_HEIGHT = (int) (VOL_BUTTON_DEFAULT_HEIGHT * SCALE);
+            public static final int VOL_SLIDER_WIDTH = (int) (VOL_SLIDER_DEFAULT_WIDTH * SCALE);
         }
     }
 }
