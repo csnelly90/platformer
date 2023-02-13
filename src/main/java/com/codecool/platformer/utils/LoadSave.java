4f7959bd6813ebd55ca6feaaab414d08d8c98957
csnelly90 <csnelly90@gmail.com>
2023-02-13 14:32:57 +0100

    add menu full background image path to LoadSave

--- src/main/java/com/codecool/platformer/utils/LoadSave.java
+++ src/main/java/com/codecool/platformer/utils/LoadSave.java
@@ -16,6 +16,7 @@ public class LoadSave {
     public static final String LEVEL_ONE_DATA = "/tiles/level_one_data_long.png";
     public static final String MENU_BUTTONS = "/gui/menu_buttons.png";
     public static final String MENU_BACKGROUND = "/gui/menu_background.png";
+    public static final String MENU_FULL_BACKGROUND = "/gui/menu_full_background.png";
     public static final String PAUSE_BACKGROUND = "/gui/pause_menu.png";
     public static final String SOUND_BUTTONS = "/gui/sound_buttons.png";
     public static final String URM_BUTTONS = "/gui/urm_buttons.png";
